0e0b8edd27016118f3a40b569313c4fa37afbb9e
Dan Allan <dallan@bnl.gov>
2019-01-16 09:35:31 -0500

    FIX: Declare numpy requirement introduced by 7e549fde.

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
+from os import path
 import versioneer
 from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [line for line in requirements_file.read().splitlines()
+                    if not line.startswith('#')]
 
 
 setup(name='event_model',
@@ -9,5 +18,5 @@ setup(name='event_model',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       package_data={'event_model': ['schemas/*.json']},
-      install_requires=['jsonschema'],
+      install_requires=requirements,
       include_package_data=True)
