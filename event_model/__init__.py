35ab214d20c9fba236ec7f0016125b949787f82b
Dan Allan <dallan@bnl.gov>
2020-03-04 11:19:51 -0500

    Add replay utility.

--- event_model/__init__.py
+++ event_model/__init__.py
@@ -1822,3 +1822,42 @@ class NumpyEncoder(json.JSONEncoder):
                 return obj.item()
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+
+def replay(gen, callback, burst=False):
+    """
+    Emit documents to a callback with realistic time spacing.
+
+    Parameters
+    ----------
+    gen: iterable
+        Expected to yield (name, doc) pairs
+    callback: callable
+        Expected signature: callback(name, doc)
+    burst: bool, optional
+        If True, emit as fast as possible, ignoring timing. False by default.
+    """
+    DOCUMENTS_WITHOUT_A_TIME = {'datum', 'datum_page', 'resource'}
+    cache = collections.deque()
+    name, doc = next(gen)
+    if name != 'start':
+        raise ValueError("Expected gen to start with a RunStart document")
+    # Compute time difference between now and the time that this run started.
+    offset = ttime.time() - doc['time']
+    callback(name, doc)
+    for name, doc in gen:
+        if name in DOCUMENTS_WITHOUT_A_TIME:
+            # The bluesky RunEngine emits these documents immediately
+            # before emitting an Event, which does have a time. Lacking
+            # more specific timing info, we'll cache these and then emit
+            # them in a burst before the next document with an associated time.
+            cache.append((name, doc))
+        else:
+            if not burst:
+                delay = max(0, offset - (ttime.time() - doc['time']))
+                ttime.sleep(delay)
+            while cache:
+                # Emit any cached documents without a time in a burst.
+                callback(*cache.popleft())
+            # Emit this document.
+            callback(name, doc)
