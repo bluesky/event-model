01ff096b9f5e537aa748319fa73f0e92814327b7
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-07-28 13:40:25 -0700

    simplified test

--- event_model/tests/test_projections.py
+++ event_model/tests/test_projections.py
@@ -4,26 +4,11 @@ from jsonschema.exceptions import ValidationError
 
 def test_projection_start_doc():
     metadata = {'so_long': 'thanks_for_all_the_fish'}
-    # proejctions are added to metadata which is added to start document, so
-    # we have several paths to test here
-
-    # 1 add proection without metadata
-    run_bundle = event_model.compose_run(uid="42", projections=valid_projections)
+    run_bundle = event_model.compose_run(uid="42", metadata={"projections": valid_projections})
     start_doc = run_bundle.start_doc 
     assert start_doc['projections'] == valid_projections
 
-    # 2 add no projection    
-    run_bundle = event_model.compose_run(uid="42", metadata=metadata)
-    start_doc = run_bundle.start_doc 
-    assert 'projections' not in start_doc
-    assert start_doc['so_long'] == 'thanks_for_all_the_fish'
 
-
-    # 3 add projection with  metadata
-    run_bundle = event_model.compose_run(uid="42", metadata=metadata, projections=valid_projections)
-    start_doc = run_bundle.start_doc 
-    assert start_doc['projections'] == valid_projections
-    assert start_doc['so_long'] == 'thanks_for_all_the_fish'
 
 
 def test_projection_schema():
