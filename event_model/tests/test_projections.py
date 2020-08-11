41af2e0f05418f5f94c65e53550852c70d516940
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-08-11 12:07:01 -0700

    flake8 fixes

--- event_model/tests/test_projections.py
+++ event_model/tests/test_projections.py
@@ -2,13 +2,11 @@ import event_model
 import pytest
 from jsonschema.exceptions import ValidationError
 
+
 def test_projection_start_doc():
-    metadata = {'so_long': 'thanks_for_all_the_fish'}
     run_bundle = event_model.compose_run(uid="42", metadata={"projections": valid_projections})
-    start_doc = run_bundle.start_doc 
+    start_doc = run_bundle.start_doc
     assert start_doc['projections'] == valid_projections
-
-
 
 
 def test_projection_schema():
@@ -16,8 +14,8 @@ def test_projection_schema():
     event_model.schema_validators[event_model.DocumentNames.start].validate(start_doc)
 
     with pytest.raises(ValidationError):
-            start_doc['projections'] = invalid_projections
-            event_model.schema_validators[event_model.DocumentNames.start].validate(start_doc)
+        start_doc['projections'] = invalid_projections
+        event_model.schema_validators[event_model.DocumentNames.start].validate(start_doc)
 
 
 valid_projections = [
@@ -25,12 +23,11 @@ valid_projections = [
                 "name": "test",
                 "version": "42.0.0",
                 "configuration": {},
-                
                 "projection": {
                     'entry/instrument/detector/data': {
                         'type': 'linked',
-                        'location': 'event', 
-                        'stream': 'primary', 
+                        'location': 'event',
+                        'stream': 'primary',
                         'field': 'ccd',
                     },
                     '/entry/instrument/wavelength': {
@@ -43,7 +40,6 @@ valid_projections = [
                 },
             }
         ]
-    
 
 invalid_projections = [
                 {
@@ -52,14 +48,15 @@ invalid_projections = [
                     "configuration": {},
                     "projection": {
                         'entry/instrument/detector/data': {
-                            'location': 'THIS IS NOT VALID', 
-                            'stream': 'primary', 
+                            'location': 'THIS IS NOT VALID',
+                            'stream': 'primary',
                             'field': 'ccd',
                         },
                     },
                 }
             ]
+
 start_doc = {
     "uid": "abc",
     "time": 0,
-} 
+}
