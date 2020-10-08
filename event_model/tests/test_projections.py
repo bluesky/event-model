96566f56ec9d6656ece727ffc5b864f9b3910886
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-10-08 09:21:42 -0700

    add configuration fields and improve conditions

--- event_model/tests/test_projections.py
+++ event_model/tests/test_projections.py
@@ -12,23 +12,23 @@ def start():
 def test_projection_in_start_doc():
     run_bundle = event_model.compose_run(uid="42", metadata={"projections": valid_projections})
     start_doc = run_bundle.start_doc
-    assert start_doc['projections'] == valid_projections
+    assert start_doc["projections"] == valid_projections
 
 
 def test_projection_schema(start):
-    start['projections'] = valid_projections
+    start["projections"] = valid_projections
     event_model.schema_validators[event_model.DocumentNames.start].validate(start)
 
     with pytest.raises(ValidationError):
-        start['projections'] = invalid_projections
+        start["projections"] = invalid_projections
         event_model.schema_validators[event_model.DocumentNames.start].validate(start)
 
     invalid_calc_projections = valid_projections.copy()
-    invalid_calc_projections[0]['projection']['calc_field'] = {'linked_field': {
-                                                        'type': 'linked',
-                                                        'location': 'event',
-                                                        'stream': 'primary',
-                                                        'field': 'ccd',
+    invalid_calc_projections[0]["projection"]["calc_field"] = {"linked_field": {
+                                                        "type": "linked",
+                                                        "location": "event",
+                                                        "stream": "primary",
+                                                        "field": "ccd",
                                                         }}  # calc requires the calc fields
     with pytest.raises(ValidationError, ):
         event_model.schema_validators[event_model.DocumentNames.start].validate(start)
@@ -40,18 +40,29 @@ valid_projections = [
                 "version": "42.0.0",
                 "configuration": {},
                 "projection": {
-                    'linked_field': {
-                        'type': 'linked',
-                        'location': 'event',
-                        'stream': 'primary',
-                        'field': 'ccd',
+                    "linked_field": {
+                        "type": "linked",
+                        "location": "event",
+                        "stream": "primary",
+                        "field": "ccd",
                     },
-                    'calc_field': {
-                        'type': 'calculated',
-                        'calculation': {
-                            'callable': 'pizza.order:slice',
-                            'kwargs': {'toppings': 'cheese'}
+                    "calc_field": {
+                        "type": "calculated",
+                        "location": "event",
+                        "field": "calc_field",
+                        "stream": "calc_stream",
+                        "calculation": {
+                            "callable": "pizza.order:slice",
+                            "kwargs": {"toppings": "cheese"}
                         }
+                    },
+                    "config_field": {
+                        "type": "linked",
+                        "location": "configuration",
+                        "config_index": 0,
+                        "config_device": "camera",
+                        "stream": "primary",
+                        "field": "setting"
                     }
                 },
             }
@@ -63,10 +74,10 @@ invalid_projections = [
                     "version": "42.0.0",
                     "configuration": {},
                     "projection": {
-                        'entry/instrument/detector/data': {
-                            'location': 'THIS IS NOT VALID',
-                            'stream': 'primary',
-                            'field': 'ccd',
+                        "entry/instrument/detector/data": {
+                            "location": "THIS IS NOT VALID",
+                            "stream": "primary",
+                            "field": "ccd",
                         },
                     },
                 }
