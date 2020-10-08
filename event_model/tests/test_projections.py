82318552fb07e8e080b196ddc85ee2f338022a16
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-10-08 10:24:25 -0700

    projecions: improved validation and testing

--- event_model/tests/test_projections.py
+++ event_model/tests/test_projections.py
@@ -19,17 +19,80 @@ def test_projection_schema(start):
     start["projections"] = valid_projections
     event_model.schema_validators[event_model.DocumentNames.start].validate(start)
 
-    with pytest.raises(ValidationError):
-        start["projections"] = invalid_projections
+
+def test_bad_calc_field(start):
+    bad_calc_projections = [
+                # calc requires the calc fields
+                {
+                    "name": "test",
+                    "version": "42.0.0",
+                    "configuration": {},
+                    "projection": {
+                                "linked_field": {
+                                    "type": "calculated",
+                                    "location": "event",
+                                    "stream": "primary",
+                                    "field": "ccd",
+                                    }, }, }, ]
+
+    start["projections"] = bad_calc_projections
+    with pytest.raises(ValidationError, ):
         event_model.schema_validators[event_model.DocumentNames.start].validate(start)
 
-    invalid_calc_projections = valid_projections.copy()
-    invalid_calc_projections[0]["projection"]["calc_field"] = {"linked_field": {
-                                                        "type": "linked",
-                                                        "location": "event",
-                                                        "stream": "primary",
-                                                        "field": "ccd",
-                                                        }}  # calc requires the calc fields
+
+def test_bad_configuration_field(start):
+    bad_configuration_projections = [
+                {
+                    "name": "test",
+                    "version": "42.0.0",
+                    "configuration": {},
+                    "projection": {
+                              "bad_config_field": {
+                                "type": "calculated",
+                                "location": "event",
+                                "config_index": 0,
+                                "config_device": "camera",
+                                "stream": "primary",
+                                # "field": "setting"
+                                }, }, }, ]
+
+    start["projections"] = bad_configuration_projections
+    with pytest.raises(ValidationError, ):
+        event_model.schema_validators[event_model.DocumentNames.start].validate(start)
+
+
+def test_bad_event_field(start):
+    bad_event_projections = [
+                {
+                    "name": "test",
+                    "version": "42.0.0",
+                    "configuration": {},
+                    "projection": {
+                                 "bad_event_field": {
+                                    "type": "linked",
+                                    "location": "event",
+                                    # "stream": "primary",
+                                    "field": "ccd",
+                                    }, }, }, ]
+    start["projections"] = bad_event_projections
+    with pytest.raises(ValidationError, ):
+        event_model.schema_validators[event_model.DocumentNames.start].validate(start)
+
+
+def test_bad_location_field(start):
+    bad_event_projections = [
+                {
+                    "name": "test",
+                    "version": "42.0.0",
+                    "configuration": {},
+                    "projection": {
+                                 "bad_event_field": {
+                                    "type": "linked",
+                                    "location": "event",
+                                    # "stream": "primary",
+                                    "field": "ccd",
+                                    }, }, }, ]
+    start["projections"] = bad_event_projections
     with pytest.raises(ValidationError, ):
         event_model.schema_validators[event_model.DocumentNames.start].validate(start)
 
@@ -67,18 +130,3 @@ valid_projections = [
                 },
             }
         ]
-
-invalid_projections = [
-                {
-                    "name": "test",
-                    "version": "42.0.0",
-                    "configuration": {},
-                    "projection": {
-                        "entry/instrument/detector/data": {
-                            "location": "THIS IS NOT VALID",
-                            "stream": "primary",
-                            "field": "ccd",
-                        },
-                    },
-                }
-            ]
