c11a87163ced203ebdf2f6096da3efcf814a41fb
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-12-11 13:59:26 -0800

    change from auth_session to data_session

--- event_model/tests/test_auth.py
+++ event_model/tests/test_auth.py
@@ -4,11 +4,11 @@ from jsonschema.exceptions import ValidationError
 
 
 def test_auth_session():
-    run_bundle = event_model.compose_run(uid="42", metadata={"auth_session": ['a', 'b']})
+    run_bundle = event_model.compose_run(uid="42", metadata={"data_session": ['a', 'b']})
     start_doc = run_bundle.start_doc
-    assert start_doc["auth_session"] == ['a', 'b']
+    assert start_doc["data_session"] == ['a', 'b']
 
     with pytest.raises(ValidationError):
-        run_bundle = event_model.compose_run(uid="42", metadata={"auth_session": 42})
+        run_bundle = event_model.compose_run(uid="42", metadata={"data_session": 42})
     with pytest.raises(ValidationError):
-        run_bundle = event_model.compose_run(uid="42", metadata={"auth_session": {"a": "b"}})
+        run_bundle = event_model.compose_run(uid="42", metadata={"data_session": {"a": "b"}})
