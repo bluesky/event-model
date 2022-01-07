a6b249f189ca02dba60e2ea655d0f18605c4742e
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-01-07 11:46:51 +0000

    Initial structure

--- setup.py
+++ setup.py
@@ -1,69 +1,13 @@
-from os import path
-from setuptools import setup, find_packages
-import sys
-import versioneer
+# type: ignore
+import glob
+import importlib.util
 
+from setuptools import setup
 
-# NOTE: This file must remain Python 2 compatible for the foreseeable future,
-# to ensure that we error out properly for people with outdated setuptools
-# and/or pip.
-min_version = (3, 6)
-if sys.version_info < min_version:
-    error = """
-event-model does not support Python {0}.{1}.
-Python {2}.{3} and above is required. Check your Python version like so:
+# Import <package>._version_git.py without importing <package>
+path = glob.glob(__file__.replace("setup.py", "src/*/_version_git.py"))[0]
+spec = importlib.util.spec_from_file_location("_version_git", path)
+vg = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(vg)
 
-python3 --version
-
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-
-pip install --upgrade pip
-""".format(*(sys.version_info[:2] + min_version))
-    sys.exit(error)
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
-    readme = readme_file.read()
-
-with open(path.join(here, 'requirements.txt')) as requirements_file:
-    # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
-
-
-setup(
-    name='event-model',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Data model used by the bluesky ecosystem",
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    author="Brookhaven National Lab",
-    author_email='dallan@bnl.gov',
-    url='https://github.com/NSLS-II/event-model',
-    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
-    packages=find_packages(exclude=['docs', 'tests']),
-    entry_points={
-        'console_scripts': [
-            # 'some.module:some_function',
-            ],
-        },
-    include_package_data=True,
-    package_data={
-        'event_model': [
-            # When adding files here, remember to update MANIFEST.in as well,
-            # or else they will not be included in the distribution on PyPI!
-            # 'path/to/data_file',
-            'schemas/*.json'
-            ]
-        },
-    install_requires=requirements,
-    license="BSD (3-clause)",
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-    ],
-)
+setup(cmdclass=vg.get_cmdclass(), version=vg.__version__)
