ff758fd96b4ba7e1d314c7287eb1ccc68ac45960
Thomas A Caswell <tcaswell@bnl.gov>
2022-05-10 12:00:03 -0400

    API: set minimum supported python in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import versioneer
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
 # to ensure that we error out properly for people with outdated setuptools
 # and/or pip.
-min_version = (3, 6)
+min_version = (3, 8)
 if sys.version_info < min_version:
     error = """
 event-model does not support Python {0}.{1}.
