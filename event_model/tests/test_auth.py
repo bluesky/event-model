01898f228dc365ceb766785120c707a9897eee36
Eva Lott <evalott100@gmail.com>
2023-02-28 09:22:58 +0000

    Ran black and isort

--- event_model/tests/test_auth.py
+++ event_model/tests/test_auth.py
@@ -1,16 +1,23 @@
-import event_model
 import pytest
 from jsonschema.exceptions import ValidationError
 
+import event_model
+
 
 def test_data_session():
-    run_bundle = event_model.compose_run(uid="42", metadata={"data_groups": ["a", "b"],
-                                                             "data_session": "magrathia_visit_42"})
+    run_bundle = event_model.compose_run(
+        uid="42",
+        metadata={"data_groups": ["a", "b"], "data_session": "magrathia_visit_42"},
+    )
     start_doc = run_bundle.start_doc
-    assert start_doc["data_groups"] == ['a', 'b']
+    assert start_doc["data_groups"] == ["a", "b"]
 
     with pytest.raises(ValidationError):
-        event_model.compose_run(uid="42", metadata={"data_session": ["shoud", "not", "be", "a", "list"]})
+        event_model.compose_run(
+            uid="42", metadata={"data_session": ["shoud", "not", "be", "a", "list"]}
+        )
 
     with pytest.raises(ValidationError):
-        event_model.compose_run(uid="42", metadata={"data_groups": "this should be an array"})
+        event_model.compose_run(
+            uid="42", metadata={"data_groups": "this should be an array"}
+        )
