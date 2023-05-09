fb2251c3eb7f86dda46186b1ae35dca5cc91052e
Eva Lott <evalott100@gmail.com>
2023-05-09 10:30:57 +0100

    made mypy complient

--- event_model/documents/__init__.py
+++ event_model/documents/__init__.py
@@ -1,12 +1,11 @@
-from event_model.documents.datum import Datum
-
 # flake8: noqa
-from event_model.documents.datum_page import DatumPage
-from event_model.documents.event import Event
-from event_model.documents.event_descriptor import EventDescriptor
-from event_model.documents.event_page import EventPage
-from event_model.documents.resource import Resource
-from event_model.documents.run_start import RunStart
-from event_model.documents.run_stop import RunStop
-from event_model.documents.stream_datum import StreamDatum
-from event_model.documents.stream_resource import StreamResource
+from .datum import Datum
+from .datum_page import DatumPage
+from .event import Event
+from .event_descriptor import EventDescriptor
+from .event_page import EventPage
+from .resource import Resource
+from .run_start import RunStart
+from .run_stop import RunStop
+from .stream_datum import StreamDatum
+from .stream_resource import StreamResource
