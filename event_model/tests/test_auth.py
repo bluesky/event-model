56b4f89224d1c4e2518f011d53c333a605fd30fc
Eva Lott <evalott100@gmail.com>
2023-05-24 15:12:10 +0100

    validated keys in unit test from bluesky

--- event_model/tests/test_auth.py
+++ event_model/tests/test_auth.py
@@ -1,7 +1,15 @@
+from uuid import uuid4
+
+import jsonschema
 import pytest
 from jsonschema.exceptions import ValidationError
 
 import event_model
+from event_model import DocumentNames, schema_validators
+
+
+def new_uid():
+    return str(uuid4())
 
 
 def test_data_session():
@@ -21,3 +29,47 @@ def test_data_session():
         event_model.compose_run(
             uid="42", metadata={"data_groups": "this should be an array"}
         )
+
+
+def test_dots_not_allowed_in_keys():
+    doc = {"time": 0, "uid": new_uid()}
+    schema_validators[DocumentNames.start].validate(doc)
+
+    # Add a legal key.
+    doc.update({"b": "c"})
+    schema_validators[DocumentNames.start].validate(doc)
+    # Now add illegal key.
+    doc.update({"b.": "c"})
+    with pytest.raises(jsonschema.ValidationError):
+        schema_validators[DocumentNames.start].validate(doc)
+
+    doc = {
+        "time": 0,
+        "uid": new_uid(),
+        "data_keys": {"a": {"source": "", "dtype": "number", "shape": []}},
+        "run_start": new_uid(),
+    }
+    schema_validators[DocumentNames.descriptor].validate(doc)
+    # Add a legal key.
+    doc.update({"b": "c"})
+    schema_validators[DocumentNames.descriptor].validate(doc)
+    # Now add illegal key.
+    doc.update({"b.c": "d"})
+    with pytest.raises(jsonschema.ValidationError):
+        schema_validators[DocumentNames.descriptor].validate(doc)
+
+    doc = {
+        "time": 0,
+        "uid": new_uid(),
+        "exit_status": "success",
+        "reason": "",
+        "run_start": new_uid(),
+    }
+    schema_validators[DocumentNames.stop].validate(doc)
+    # Add a legal key.
+    doc.update({"b": "c"})
+    schema_validators[DocumentNames.stop].validate(doc)
+    # Now add illegal key.
+    doc.update({".b": "c"})
+    with pytest.raises(jsonschema.ValidationError):
+        schema_validators[DocumentNames.stop].validate(doc)
