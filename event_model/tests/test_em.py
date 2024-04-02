81ace7ecc8c0340f96a0be6c511c5a60515eaefb
Eugene <ymatviych@bnl.gov>
2024-04-02 14:55:08 -0400

    Add localhost to uri
    Co-authored-by: Dan Allan <daniel.b.allan@gmail.com>

--- event_model/tests/test_em.py
+++ event_model/tests/test_em.py
@@ -108,7 +108,7 @@ def test_compose_stream_resource(tmp_path):
     assert bundle.compose_stream_resource is compose_stream_resource
     bundle = compose_stream_resource(
         mimetype="image/tiff",
-        uri="file://" + str(tmp_path) + "/test_streams",
+        uri="file://localhost" + str(tmp_path) + "/test_streams",
         data_key="det1",
         parameters={},
     )
