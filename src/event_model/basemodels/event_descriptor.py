361fab030fd87d54da399bab3a28a17b55c520da
Tom C (DLS) <101418278+coretl@users.noreply.github.com>
2025-06-25 12:44:58 +0100

    Update event_descriptor.py

--- src/event_model/basemodels/event_descriptor.py
+++ src/event_model/basemodels/event_descriptor.py
@@ -74,7 +74,7 @@ class RdsRange(BaseModel):
 class Limits(BaseModel):
     """
     Epics limits:
-    see 3.4.1 https://epics.anl.gov/base/R3-14/12-docs/AppDevGuide/node4.html
+    https://docs.epics-controls.org/en/latest/getting-started/EPICS_Intro.html#channel-access
     """
 
     model_config = ConfigDict(extra="forbid")
